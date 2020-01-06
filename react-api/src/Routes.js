4c72e534cd8fd28d6d694896565bb3d817037aa1
Gergely Brautigam <gergely@arangodb.com>
2020-01-06 08:20:40 +0100

    Further work

--- react-api/src/Routes.js
+++ react-api/src/Routes.js
@@ -5,6 +5,7 @@ import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
 import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 
 export default function Routes({ appProps }) {
   return (
@@ -12,6 +13,7 @@ export default function Routes({ appProps }) {
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
       <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
--- react-api/src/containers/Signup.js
+++ react-api/src/containers/Signup.js
@@ -33,19 +33,41 @@ export default function Signup(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
-
+  
     setIsLoading(true);
-
-    setNewUser("test");
-
-    setIsLoading(false);
+  
+    try {
+    //   const newUser = await Auth.signUp({
+    //     username: fields.email,
+    //     password: fields.password
+    //   });
+      // Send user data to backend server and wait for an OK response.
+      setIsLoading(false);
+      setNewUser(newUser);
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
   }
-
+  
   async function handleConfirmationSubmit(event) {
     event.preventDefault();
-
+  
     setIsLoading(true);
-  }
+  
+    try {
+      // await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+      // await Auth.signIn(fields.email, fields.password);
+      // Generate a 6 digit token and send it via email. Store it
+      // and wait for verification.
+
+      props.userHasAuthenticated(true);
+      props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
+  }  
 
   function renderConfirmationForm() {
     return (
