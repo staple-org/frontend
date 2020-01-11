3fe0a30f56f952bb9a4db7add0a355d4c466555d
Gergely Brautigam <gergely@arangodb.com>
2020-01-11 16:17:27 +0100

    Working staple view

--- src/containers/Staples.js
+++ src/containers/Staples.js
@@ -1,14 +1,18 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import config from "../config";
 import Cookie from "js-cookie";
+import { FormGroup, FormControl } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import "./Staples.css";
 
 export default function Staples(props) {
   const [staple, setStaple] = useState(null);
   const [content, setContent] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
-    function loadStaple() {
-      // return API.get("notes", `/notes/${props.match.params.id}`);
+    async function onLoad() {
       try {
         fetch(config.HOST+`/rest/api/1/staple/${props.match.params.id}`, {
           method: 'GET',
@@ -17,19 +21,11 @@ export default function Staples(props) {
             'Authorization': 'Bearer ' + Cookie.get('token'),
           },
         }).then(response => response.json())
-          .then(data => setStaple(data))
+          .then(data => {
+            setContent(data.staple.content);
+            setStaple(data.staple);
+          })
           .catch(e => alert(e.message));
-      } catch (e) {
-        alert(e);
-      }
-    }
-
-    async function onLoad() {
-      try {
-        const staple = await loadStaple();
-        console.log(staple);
-        setContent(staple.content);
-        setStaple(staple);
       } catch (e) {
         alert(e);
       }
@@ -37,7 +33,45 @@ export default function Staples(props) {
     onLoad();
   }, [props.match.params.id]);
 
+  async function handleSubmit(event) {
+
+    event.preventDefault();
+
+    // mark as archvied here.
+    // if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+    //   alert(
+    //     `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
+    //     1000000} MB.`
+    //   );
+    //   return;
+    // }
+
+    setIsLoading(true);
+    props.history.push('/')
+  }
+
   return (
-    <div className="Notes"></div>
+    <div className="Staples">
+      {staple && (
+        <form>
+          <FormGroup controlId="content">
+            <FormControl
+              plaintext={content}
+              readOnly
+              defaultValue={content}
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            bsSize="large"
+            bsStyle="danger"
+            onClick={handleSubmit}
+            isLoading={isDeleting}
+          >
+            Archive
+          </LoaderButton>
+        </form>
+      )}
+    </div>
   );
 }
