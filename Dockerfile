01e435abd518c95039e2c36b45fd8ed7a6c93006
Gergely Brautigam <gergely@arangodb.com>
2020-02-07 21:31:02 +0100

    HOST is now an environment property

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:alpine3.11
+
+RUN npm install -g serve
+
+RUN mkdir /app
+COPY ./build /app
+
+WORKDIR /app/
+ENTRYPOINT [ "serve -s" ]
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   MAX_ATTACHMENT_SIZE: 500000,
-  HOST: "http://localhost:9998"
+  HOST: process.env.STAPLE_HOST || "http://localhost:9998"
 }
