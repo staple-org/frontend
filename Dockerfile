2fa86fe2b2ca6b0fca8f9cd4636c4476a9d648ba
Gergely Brautigam <gergely@arangodb.com>
2020-02-08 10:25:51 +0100

    Fixed the environment variable host thing

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN npm install -g serve
 
 RUN mkdir /app
 COPY ./build /app
+COPY .env /app/.env
 
 WORKDIR /app/
-ENTRYPOINT [ "/usr/local/bin/serve -s" ]
+ENTRYPOINT [ "/usr/local/bin/serve", "-s" ]
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+require('dotenv').config();
 
 ReactDOM.render(
     <Router>
